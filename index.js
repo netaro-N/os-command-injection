f7a79c06dc5576cadf0521888f2ce9189079bb9f
netaro-N <takebbb1+github@gmail.com>
2018-11-22 11:35:03 +0000

    文字化け処理

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type':'text/plain;charset=utf-8' //
+  })
   const path = req.url;
   res.end(path);
 });
